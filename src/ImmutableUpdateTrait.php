bf5b6bd34bd1bb465ad4ee43e0fb3029b70ea7dd
ARAI Takashi <takashi.arai.nz@icloud.com>
2020-03-07 15:35:15 +0900

    pass a test

--- src/ImmutableUpdateTrait.php
+++ src/ImmutableUpdateTrait.php
@@ -7,6 +7,25 @@ trait ImmutableUpdateTrait
 
     public function __call($name, $arguments)
     {
+        if (preg_match('/^with(.*)$/', $name, $matches)) {
+
+            $prop_name = $matches[1];
+
+            $prop_name = strtolower($prop_name);
+
+            if (property_exists($this, $prop_name)) {
+                $new_value = $arguments[0];
+
+                $current = get_object_vars($this);
+
+                $mixed = array_merge($current, [$prop_name => $new_value]);
+
+                $next = array_values($mixed);
+
+                return new static(...$next);
+            }
+        }
+
         throw new \BadFunctionCallException('Undefined method call:'.$name);
     }
 
--- tests/ImmutableUpdateTraitTest.php
+++ tests/ImmutableUpdateTraitTest.php
@@ -4,16 +4,43 @@ namespace Araitta\ImmutableUpdate;
 
 use PHPUnit\Framework\TestCase;
 
+class TestingClass
+{
+    use ImmutableUpdateTrait;
+
+    private $foo;
+    private $bar;
+    private $baz;
+
+    public function __construct($foo, $bar, $baz)
+    {
+        $this->foo = $foo;
+        $this->bar = $bar;
+        $this->baz = $baz;
+    }
+
+    public function dump()
+    {
+        return get_object_vars($this);
+    }
+
+}
+
 class ImmutableUpdateTraitTest extends TestCase
 {
 
     public function test()
     {
-        $obj = new class {
-            use ImmutableUpdateTrait;
-        };
+        $obj = new TestingClass("FOO", "BAR", "BAZ");
 
-        $obj->meth();
+        $new = $obj->withFoo("Ninja");
+
+        $this->assertNotSame($obj, $new, '$new is a new instance');
+        $this->assertInstanceOf(TestingClass::class, $new);
+
+        $this->assertSame("Ninja", $new->dump()['foo']);
+
+        $this->assertSame(['foo' => 'Ninja', 'bar' => "BAR", 'baz' => 'BAZ'], $new->dump());
     }
 
 }
