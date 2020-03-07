8d1875d38ead092a6288555353932e6f7e301ab1
ARAI Takashi <takashi.arai.nz@icloud.com>
2020-03-07 14:34:22 +0900

    initial

---
+++ src/ImmutableUpdateTrait.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Araitta\ImmutableUpdateTrait;
+
+trait ImmutableUpdateTrait
+{
+
+    public function __call($name, $arguments)
+    {
+    }
+
+}
+
---
+++ tests/ImmutableUpdateTraitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Araitta\ImmutableUpdateTrait;
+
+use PHPUnit\Framework\TestCase;
+
+class ImmutableUpdateTraitTest extends TestCase
+{
+
+    public function test()
+    {
+        $this->assertEquals(1, 2);
+    }
+
+}
+
